8ded0db95f4d07462ec800e68a6ec0f4c7005160
Bbock2511 <128552505+Bbock2511@users.noreply.github.com>
2023-09-26 13:41:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ajudagro/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ajudagro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.ajudagro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
